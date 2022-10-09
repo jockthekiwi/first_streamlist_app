a591bfd20fbbc99f5a4cff842fb9a7f43d9c74ca
jockthekiwi <gavin@fa.net.nz>
2022-10-09 21:51:40 +1300

    Update streamlist_app.py
    Emoticons

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,8 +1,8 @@
 import streamlit;
 
 streamlit.title('My parents new healthy Diner');
-streamlit.header('Breakfast menu');
-streamlit.text('Omega 3 & Blueberry Oatmeal');
-streamlit.text('Kale, Spinach & Rocket Smoothie');
-streamlit.text('Hard-Boiled Free-Range Egg');
+streamlit.header('🍞 Breakfast menu');
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal');
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie');
+streamlit.text('🥗 Hard-Boiled Free-Range Egg');
 
